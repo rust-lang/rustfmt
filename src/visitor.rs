60a1414a72f9c0200c2680c0a8a02887b883d631
Nick Cameron <ncameron@mozilla.com>
2016-05-18 11:15:07 +1200

    Optionally put short struct variants on one line
    Closes #418

--- src/visitor.rs
+++ src/visitor.rs
@@ -246,7 +246,8 @@ impl<'a> FmtVisitor<'a> {
                                            def,
                                            Some(generics),
                                            item.span,
-                                           indent)
+                                           indent,
+                                           None)
                         .map(|s| {
                             match *def {
                                 ast::VariantData::Tuple(..) => s + ";",
