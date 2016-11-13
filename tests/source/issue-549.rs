e7b0ecf705947d19771622619a3fb6a047bfc953
bekh <6ex@mail.ru>
2016-11-13 17:38:33 +0100

    Added tests for `format!` like macros formatting

---
+++ tests/source/issue-549.rs
@@ -0,0 +1,13 @@
+fn main() {
+    format!("This is one {} loooooooooooooooooooooooooooooooooooooooooooooooooooooong {} for {}", "very", "string", "test");
+
+    format!("Many arguments: {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}", "arg1", "arg2", "arg3", "arg4", "arg5", "arg6", "arg7", "arg8", "arg9", "arg10", "arg11", "arg12", "arg13", "arg14", "arg15");
+
+    format_args!("This is one {} looooooooooooooooooooooooooooooooooooooooooooooooong {} for {}", "very", "string", "test");
+    
+    panic!("This is one {} loooooooooooooooooooooooooooooooooooooooooooooooooooooong {} for {}", "very", "string", "test");
+    
+    print!("This is one {} loooooooooooooooooooooooooooooooooooooooooooooooooooooong {} for {}", "very", "string", "test");
+    
+    println!("This is one {} loooooooooooooooooooooooooooooooooooooooooooooooooooooong {} for {}", "very", "string", "test");
+}
