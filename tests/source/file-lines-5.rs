dd142d0991bbd425976ea8851a6d41edb5a27be5
sinkuu <sinkuupump@gmail.com>
2017-02-26 18:50:31 +0900

    Add tests

---
+++ tests/source/file-lines-5.rs
@@ -0,0 +1,26 @@
+// rustfmt-file_lines: [{"file":"tests/source/file-lines-5.rs","range":[9,10]},{"file":"tests/source/file-lines-5.rs","range":[20,25]}]
+// rustfmt-error_on_line_overflow: false
+
+impl A {
+    fn foo() {
+    }
+}
+
+impl B {
+    fn foo() {
+    }
+}
+
+trait C {
+    fn foo() {
+    }
+}
+
+fn main() {
+    let y = if cond {
+            val1
+        } else {
+            val2
+        }
+            .method_call();
+}
