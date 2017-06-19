99336c318bc1a4b87cbcdc60f8dbb5358deb0a77
Ian Pickering <ipickering2@gmail.com>
2017-06-19 13:27:01 -0500

    fix test

--- tests/target/configs-match_arm_align_threshold-below.rs
+++ tests/target/configs-match_arm_align_threshold-below.rs
@@ -3,16 +3,16 @@
 
 fn main() {
     match lorem {
-        Lorem::Ipsum                                                                    => (),
-        Lorem::DolorSitAmetConsecteturAdipiscingElitSedDo                               => (),
-        Lorem::Eiusmod                                                                  => {
+        Lorem::Ipsum                                                                     => (),
+        Lorem::DolorSitAmetConsecteturAdipiscingElitSedDo                                => (),
+        Lorem::Eiusmod                                                                   => {
             lorem();
             ipsum();
         }
         Lorem::Donec | Lorem::Hendrerit | Lorem::Tempor | Lorem::Tellus | Lorem::Proin |
-        Lorem::Quam | Lorem::Nisl                                                       => (),
+        Lorem::Quam | Lorem::Nisl                                                        => (),
         Lorem::DonecHendrerit |
-        Lorem::TemporTellusProinQuamNislTinciduntEtMattisEgetConvallisNecPurusCumSociis => (),
-        Lorem::Natoque if lorem()                                                       => (),
+        Lorem::TemporTellusProinQuamNislTinciduntEtMattisEgetConvallisNecPurusCumSociis  => (),
+        Lorem::Natoque if lorem()                                                        => (),
     }
 }
