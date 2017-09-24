25b6a6bc65916e9631a5336736a648084ff89c54
Afshin Mehrabani <afshin.meh@gmail.com>
2017-09-24 12:24:48 +0100

    fix(where): adding more tests

--- tests/source/configs-where_single_line.rs
+++ tests/source/configs-where_single_line.rs
@@ -1,6 +1,10 @@
 // rustfmt-where_single_line: true
 // Where style
 
+fn lorem_two_items<Ipsum, Dolor, Sit, Amet>() -> T where Ipsum: Eq, Lorem: Eq {
+    // body
+}
+
 fn lorem<Ipsum, Dolor, Sit, Amet>() -> T where Ipsum: Eq {
     // body
 }
