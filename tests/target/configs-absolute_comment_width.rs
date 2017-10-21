7a2c74c379a1fbbca68626e7b4eab96624c9d396
Matthew McAllister <mattmcal@ocf.berkeley.edu>
2017-10-20 23:03:17 -0700

    Add absolute_comment_width setting

---
+++ tests/target/configs-absolute_comment_width.rs
@@ -0,0 +1,16 @@
+// rustfmt-wrap_comments: true
+// rustfmt-comment_width: 72
+// rustfmt-absolute_comment_width: true
+impl MyStruct {
+    fn my_fun() {
+        if foo {
+            // Please wrap this comment at column number 72 exactly,
+            // rather than column 84
+        }
+
+        fn bar() {
+            //! Please wrap this comment at column number 72 exactly,
+            //! rather than column 84
+        }
+    }
+}
