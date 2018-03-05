73b06bbde3c540abf4500ae41f5883423c2a8479
csmoe <35686186+csmoe@users.noreply.github.com>
2018-03-05 21:34:03 +0800

    Revert "Merge branch 'struct_macro_trailing_comma' of https://github.com/csmoe/rustfmt into struct_macro_trailing_comma"
    This reverts commit 21916b99191bd2a7ca42387949528003f5113cef.

---
+++ rustfmt-core/tests/target/issue-2445.rs
@@ -0,0 +1,12 @@
+test!(RunPassPretty {
+    // comment
+    path: "src/test/run-pass/pretty",
+    mode: "pretty",
+    suite: "run-pass",
+    default: false,
+    host: true // comment
+});
+
+test!(Test {
+    field: i32 // comment
+});
