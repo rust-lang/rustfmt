71f57b2bfc8a0c9a561bd7dac2c46e3de4485fc4
calebcartwright <caleb.cartwright@outlook.com>
2019-06-26 19:05:52 -0500

    fix: only replace backslash in ignore paths on windows

--- src/ignore_path.rs
+++ src/ignore_path.rs
@@ -11,7 +11,12 @@ impl IgnorePathSet {
         let mut ignore_builder = gitignore::GitignoreBuilder::new(ignore_list.rustfmt_toml_path());
 
         for ignore_path in ignore_list {
-            ignore_builder.add_line(None, &ignore_path.to_str().unwrap().replace("\\", "/"))?;
+            let ignore_line = ignore_path.to_str().unwrap();
+            if cfg!(windows) {
+                ignore_builder.add_line(None, &ignore_line.replace("\\", "/"))?;
+            } else {
+                ignore_builder.add_line(None, ignore_line)?;
+            };
         }
 
         Ok(IgnorePathSet {
@@ -52,12 +57,16 @@ mod test {
                 assert!(ignore_path_set.is_match(&FileName::Real(PathBuf::from("src/foo.rs"))));
                 assert!(ignore_path_set.is_match(&FileName::Real(PathBuf::from("bar_dir/baz.rs"))));
                 assert!(!ignore_path_set.is_match(&FileName::Real(PathBuf::from("src/bar.rs"))));
-                assert!(
-                    ignore_path_set.is_match(&FileName::Real(PathBuf::from("foo/bar/abc/true.rs")))
-                );
-                assert!(
-                    !ignore_path_set.is_match(&FileName::Real(PathBuf::from("foo/bar/false.rs")))
-                );
+                if cfg!(windows) {
+                    assert!(
+                        ignore_path_set
+                            .is_match(&FileName::Real(PathBuf::from("foo/bar/abc/true.rs")))
+                    );
+                    assert!(
+                        !ignore_path_set
+                            .is_match(&FileName::Real(PathBuf::from("foo/bar/false.rs")))
+                    );
+                }
             }
             _ => (),
         };
