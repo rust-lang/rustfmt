ca9ffccf64330b5916f3d2604ef3354fe1f11f2a
Stéphane Campinas <stephane.campinas@gmail.com>
2019-07-12 15:23:28 +0200

    add tests with type_punctuation_density set to Compressed and a list of types spanning multiple lines

---
+++ tests/source/types_compressed.rs
@@ -0,0 +1,7 @@
+// rustfmt-type_punctuation_density: Compressed
+
+pub fn do_something<'a, T: Trait1 + Trait2 + 'a>(
+    &fooo: u32,
+) -> impl Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + Foo + 'a + 'a + 'a + 'a + 'a
+ {
+}
