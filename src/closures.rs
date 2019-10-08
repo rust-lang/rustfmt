1d60cb9603d0d7986944d190a35311987fc272c1
rchaser53 <tayoshizawa29@gmail.com>
2019-10-08 11:20:00 +0900

    use get_attrs_from_stmt

--- src/closures.rs
+++ src/closures.rs
@@ -1,6 +1,7 @@
 use syntax::source_map::Span;
 use syntax::{ast, ptr};
 
+use crate::attr::get_attrs_from_stmt;
 use crate::config::lists::*;
 use crate::config::Version;
 use crate::expr::{block_contains_comment, is_simple_block, is_unsafe_block, rewrite_cond};
@@ -103,21 +104,13 @@ fn get_inner_expr<'a>(
 
 // Figure out if a block is necessary.
 fn needs_block(block: &ast::Block, prefix: &str, context: &RewriteContext<'_>) -> bool {
-    let has_attribute = if let Some(first_stmt) = block.stmts.first() {
-        match first_stmt.kind {
-            ast::StmtKind::Local(ref local) => !local.attrs.is_empty(),
-            ast::StmtKind::Item(ref item) => !item.attrs.is_empty(),
-            ast::StmtKind::Expr(ref expr) => !expr.attrs.is_empty(),
-            ast::StmtKind::Mac(ref mac) => !mac.2.is_empty(),
-            ast::StmtKind::Semi(ref expr) => !expr.attrs.is_empty(),
-        }
-    } else {
-        false
-    };
+    let has_attributes = block.stmts.first().map_or(false, |first_stmt| {
+        !get_attrs_from_stmt(first_stmt).is_empty()
+    });
 
     is_unsafe_block(block)
         || block.stmts.len() > 1
-        || has_attribute
+        || has_attributes
         || block_contains_comment(block, context.source_map)
         || prefix.contains('\n')
 }
