4967523cb0a2c6881b5b74beb8c1ca626e9b86df
Seiichi Uchida <seuchida@gmail.com>
2019-10-26 10:18:15 +0900

    Add --locked (#3889)

--- ci/integration.sh
+++ ci/integration.sh
@@ -15,7 +15,7 @@ set -ex
 # it again.
 #
 #which cargo-fmt || cargo install --force
-cargo install --path . --force 
+cargo install --path . --force --locked
 
 echo "Integration tests for: ${INTEGRATION}"
 cargo fmt -- --version
