c6450177e4fbde5d191e2b18d000b9043388328f
Chris Emerson <github@mail.nosreme.org>
2019-10-28 22:02:21 +0000

    Allow --check with stdin.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -73,10 +73,6 @@ pub enum OperationError {
     /// An io error during reading or writing.
     #[fail(display = "io error: {}", _0)]
     IoError(IoError),
-    /// Attempt to use --check with stdin, which isn't currently
-    /// supported.
-    #[fail(display = "The `--check` option is not supported with standard input.")]
-    CheckWithStdin,
     /// Attempt to use --emit with a mode which is not currently
     /// supported with stdandard input.
     #[fail(display = "Emit mode {} not supported with standard output.", _0)]
@@ -251,20 +247,20 @@ fn format_string(input: String, options: GetOptsOptions) -> Result<i32, FailureE
     let (mut config, _) = load_config(Some(Path::new(".")), Some(options.clone()))?;
 
     if options.check {
-        return Err(OperationError::CheckWithStdin.into());
-    }
-    match options.emit_mode {
-        // Emit modes which work with standard input
-        // None means default, which is Stdout.
-        None | Some(EmitMode::Stdout) | Some(EmitMode::Checkstyle) | Some(EmitMode::Json) => {}
-        Some(emit_mode) => {
-            return Err(OperationError::StdinBadEmit(emit_mode).into());
+        config.set().emit_mode(EmitMode::Diff);
+    } else {
+        match options.emit_mode {
+            // Emit modes which work with standard input
+            // None means default, which is Stdout.
+            None | Some(EmitMode::Stdout) | Some(EmitMode::Checkstyle) | Some(EmitMode::Json) => {}
+            Some(emit_mode) => {
+                return Err(OperationError::StdinBadEmit(emit_mode).into());
+            }
         }
+        config
+            .set()
+            .emit_mode(options.emit_mode.unwrap_or(EmitMode::Stdout));
     }
-    // emit mode is always Stdout for Stdin.
-    config
-        .set()
-        .emit_mode(options.emit_mode.unwrap_or(EmitMode::Stdout));
     config.set().verbose(Verbosity::Quiet);
 
     // parse file_lines
