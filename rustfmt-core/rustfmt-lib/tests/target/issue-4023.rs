c269627e0f160771f5e8a7375d38e08a2a6ea990
rchaser53 <tayoshizawa29@gmail.com>
2020-02-10 00:09:48 +0900

    fix not to break relative path link

---
+++ rustfmt-core/rustfmt-lib/tests/target/issue-4023.rs
@@ -0,0 +1,12 @@
+// rustfmt-max_width: 100
+// rustfmt-wrap_comments: true
+// rustfmt-comment_width: 80
+// rustfmt-normalize_comments: true
+// rustfmt-unstable_features: true
+
+struct Test {
+    /// [`MANAGE_MESSAGES`]: ../permissions/struct.Permissions.html#associatedconstant.MANAGE_MESSAGES
+    /// [`MANAGE_CHANNEL`]: ./permissions/struct.Permissions.html#associatedconstant.MANAGE_CHANNEL
+    #[serde(default, rename = "rate_limit_per_user")]
+    pub rate_limit: u16,
+}
