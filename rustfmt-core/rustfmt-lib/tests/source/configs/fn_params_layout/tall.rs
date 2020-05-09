24e32971fe3a7f3368c6ca3185436c367bc1c477
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2020-05-09 10:14:36 -0700

    Rename fn_args_layout config option to fn_params_layout
    See #4149 for more details. This change is targeted for rustfmt 2.0.
    
    Closes #4149

--- rustfmt-core/rustfmt-lib/tests/source/configs/fn_params_layout/tall.rs
+++ rustfmt-core/rustfmt-lib/tests/source/configs/fn_params_layout/tall.rs
@@ -1,5 +1,5 @@
-// rustfmt-fn_args_layout: Vertical
-// Function arguments density
+// rustfmt-fn_params_layout: Tall
+// Function parameter density
 
 trait Lorem {
     fn lorem(ipsum: Ipsum, dolor: Dolor, sit: Sit, amet: Amet);
