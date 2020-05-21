e25e91f9cb0523a2dbf710c17b6902d8ddfdb36e
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2020-05-20 18:33:24 -0700

    fixup! Add test cases for fixed issues

--- rustfmt-core/rustfmt-lib/tests/target/issue-3068.rs
+++ rustfmt-core/rustfmt-lib/tests/target/issue-3068.rs
@@ -1,12 +1,13 @@
 // rustfmt-hard_tabs: true
+// rustfmt-single_line_if_else_max_width: 10
 
 macro_rules! foo {
 	($bar: expr, $t: ty) => {
 		$bar(|x| {
 			if x {
-				None;
+				None
 			} else {
-				None;
+				None
 			}
 		})
 	};
