3626a0b33e5e856450bb1a7ef7d29ab3072375a5
hafiz <20735482+ayazhafiz@users.noreply.github.com>
2020-05-20 20:38:06 -0500

    Add test cases for fixed issues (#4189)

---
+++ rustfmt-core/rustfmt-lib/tests/target/issue-3550.rs
@@ -0,0 +1,39 @@
+fn main() {
+    assert(
+        long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long(),
+    );
+
+    assert!(
+        long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long()
+            .long(),
+    );
+}
