06e66e17243153197c6e4dd6dd6417f8410cc0dd
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2020-05-26 19:52:03 -0700

    Properly skip macros in impl and trait blocks
    Closes #4167

---
+++ rustfmt-core/rustfmt-lib/tests/target/issue-4167.rs
@@ -0,0 +1,35 @@
+impl Type {
+    #[rustfmt::skip::macros(macros)]
+    fn example() {
+        func("format me");
+        other!("format me");
+        macros!(
+            "do not format"
+        );
+        macros![
+            "do not format"
+        ];
+    }
+}
+
+impl Trait for Type {
+    #[rustfmt::skip::macros(macros)]
+    fn example() {
+        func("format me");
+        other!("format me");
+        macros!(
+            "do not format"
+        );
+    }
+}
+
+trait Trait {
+    #[rustfmt::skip::macros(macros)]
+    fn example() {
+        func("format me");
+        other!("format me");
+        macros!(
+            "do not format"
+        );
+    }
+}
