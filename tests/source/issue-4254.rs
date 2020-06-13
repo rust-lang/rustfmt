74fb8cdd39d2f11b11823a10ddfc54aa2cc9f5b3
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2020-06-13 14:13:04 -0700

    Regression test for #4254
    Closes #4254

---
+++ tests/source/issue-4254.rs
@@ -0,0 +1,11 @@
+mod d;
+mod f;
+mod s;
+mod r#if;
+mod r#else;
+
+use d::*;
+use f::*;
+use s::*;
+use r#if::*;
+use r#else::*;
---
+++ tests/target/issue-4254.rs
@@ -0,0 +1,11 @@
+mod d;
+mod r#else;
+mod f;
+mod r#if;
+mod s;
+
+use d::*;
+use r#else::*;
+use f::*;
+use r#if::*;
+use s::*;
