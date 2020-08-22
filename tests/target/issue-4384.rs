ca8d96ea05d3b2eccf9f6050cee7fcc7a3136fa6
Teddy Sudol <Solumin@users.noreply.github.com>
2020-08-22 14:53:53 -0700

    Added test cases

---
+++ tests/target/issue-4384.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let v: Vec<i32> = vec![1, 2, 3, 4];
+
+    v.iter()
+        .filter(|x| {
+            // bla bla
+            **x != 2
+        })
+        .for_each(|x| println!("{}", x));
+}
