1e227bf0933d4df188e6e25ea58a5f33586bc4df
David Bar-On <david.cdb004@gmail.com>
2020-08-24 15:22:22 +0300

    Add test cases file for issue #4392

---
+++ tests/source/issue-4392.rs
@@ -0,0 +1,47 @@
+/* No new line should be added after the operation ('==', etc.) */
+
+fn main() {
+    if 'a' == b /* x */
+    as char {}
+}
+
+fn main() {
+    if 'a' == 0
+    /* x */ as char {} }
+
+fn main() {
+    if 1234 == 0
+    /* x */ & cc & dd {} }
+
+fn main() {
+    if 1234 == 'a'
+    /* x */ as i32 {} }
+
+fn main() {
+    if 0 == 'a'
+    /* x */ as i32 {} }
+
+fn main() {
+    if 'a' == b /* x */ as char { println!("Match"); }
+}
+
+fn main() {
+	while aaaaaa < bbbbb
+	as char /* x */ {} }
+	
+fn main() {
+    if 'a' == b // x
+    as char {}
+}
+
+fn main() {
+    if 'a' == /* x */ b
+    as char {}
+}
+
+fn main() {
+    if 'a' == b
+	as char {}
+	
+	let a = 5;	// inline
+}
