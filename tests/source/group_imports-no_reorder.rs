4f39fc497e4b83b6b64637c05ef8e4e8292e3169
Matthieu Felix <matthieufelix@gmail.com>
2020-10-01 18:24:54 -0400

    Add extra tests
    Tests for interaction with `no_reorder` and `merge_imports`.

---
+++ tests/source/group_imports-no_reorder.rs
@@ -0,0 +1,19 @@
+// rustfmt-group_imports: StdExternalCrate
+// rustfmt-reorder_imports: false
+
+// group_imports has no effect when reorder_imports is disabled.
+
+use chrono::Utc;
+use super::update::convert_publish_payload;
+
+use juniper::{FieldError, FieldResult};
+use uuid::Uuid;
+use alloc::alloc::Layout;
+
+use std::sync::Arc;
+
+use broker::database::PooledConnection;
+
+use super::schema::{Context, Payload};
+use core::f32;
+use crate::models::Event;
