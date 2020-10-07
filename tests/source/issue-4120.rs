f0f47f9b63ce88eee5ef9735dba220ecf48c8465
WhizSid <whizsid@aol.com>
2020-10-08 01:55:04 +0530

    Added test cases

---
+++ tests/source/issue-4120.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let x = if true {
+
+        1
+              // In if
+    } else {
+        0
+		// in else
+    };
+
+    for i in 0..2 {
+	println!("Something");
+		// in for
+    }
+
+    extern "C" {
+	fn first();
+
+	// TODO: add rest
+    }
+}
