6faca22105bc51186ffed123cfebdc6a448d8c84
WhizSid <whizsid@aol.com>
2020-10-10 16:15:46 +0530

    honor the authors intent

--- tests/source/issue-4120.rs
+++ tests/source/issue-4120.rs
@@ -1,36 +1,80 @@
 fn main() {
     let x = if true {
-
         1
-              // In if
+            // In if
     } else {
         0
-		// In else
+            // In else
     };
 
     let x = if true {
-
         1
-              /* In if */
+             /* In if */
     } else {
         0
-               /* In else */
+             /* In else */
     };
 
+    let z = if true {
+        if true {
+            1
+
+                 // In if level 2
+        } else {
+            2
+        }
+    } else {
+        3
+    };
+
+    let a = if true {
+        1
+  // In if
+    } else {
+        0
+  // In else
+    };
+
+    let a = if true {
+        1
+
+    // In if
+    } else {
+        0
+    // In else
+    };
+
+    let b = if true {
+        1
+
+    // In if
+    } else {
+        0
+        // In else
+    };
+
+    let c = if true {
+        1
+
+        // In if
+    } else {
+        0
+        // In else
+    };
     for i in 0..2 {
-	println!("Something");
-		// In for
+        println!("Something");
+        // In for
     }
 
     for i in 0..2 {
         println!("Something");
-                /* In for */
+        /* In for */
     }
 
     extern "C" {
-	fn first();
+        fn first();
 
-	// In foreign mod
+        // In foreign mod
     }
 
     extern "C" {
--- tests/target/issue-4120.rs
+++ tests/target/issue-4120.rs
@@ -15,6 +15,52 @@ fn main() {
         /* In else */
     };
 
+    let z = if true {
+        if true {
+            1
+
+            // In if level 2
+        } else {
+            2
+        }
+    } else {
+        3
+    };
+
+    let a = if true {
+        1
+    // In if
+    } else {
+        0
+        // In else
+    };
+
+    let a = if true {
+        1
+
+    // In if
+    } else {
+        0
+        // In else
+    };
+
+    let b = if true {
+        1
+
+    // In if
+    } else {
+        0
+        // In else
+    };
+
+    let c = if true {
+        1
+
+        // In if
+    } else {
+        0
+        // In else
+    };
     for i in 0..2 {
         println!("Something");
         // In for
