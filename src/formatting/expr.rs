ac7e2588219ddd363d638b8aa5c0e9cfaaf1914c
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-11-27 21:18:39 -0600

    feat: support underscore expressions

--- src/formatting/expr.rs
+++ src/formatting/expr.rs
@@ -414,6 +414,7 @@ pub(crate) fn format_expr(
             }
         }
         ast::ExprKind::Await(_) => rewrite_chain(expr, context, shape),
+        ast::ExprKind::Underscore => Some("_".to_owned()),
         ast::ExprKind::Err => None,
     };
 
