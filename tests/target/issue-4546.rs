291f980dedf2ce473886247c12fef4baf6bcf2ea
David Bar-On <david.cdb004@gmail.com>
2020-12-02 13:53:27 +0200

    Fix for issue #4546 - proper indent multiline post-comment in a list

---
+++ tests/target/issue-4546.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let v = [
+        "A",   /* item A comment */
+        "BBB", /* item B comment */
+        "CCCCCC", /* item C comment line 1
+                   * item C comment line 2 */
+        "D", /* item D comment line 1
+              * item D comment line 2 */
+        "EEEEE", /* item E comment */
+        "FFF",   /* item F comment */
+        "GG", /* item G comment line 1
+               * item G comment line 2 */
+    ];
+}
+
+fn main() {
+    let v = [
+        "GG", /* item G comment line 1
+               * item G comment line 2 */
+        "AAAAA", /* item A comment */
+        "BBB",   /* item B comment */
+        "CCCCCC", /* item C comment line 1
+                   * item C comment line 2 */
+        "D", /* item D comment line 1
+              * item D comment line 2 */
+        "E",   /* item E comment */
+        "FFF", /* item F comment */
+    ];
+}
