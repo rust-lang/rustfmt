bdab80e4f7d30b67db8aa35a32339650a0463408
Geoffry Song <goffrie@dropbox.com>
2020-12-16 11:24:33 -0800

    add a test case for merge+unnest_imports

---
+++ tests/source/merge_and_unnest_imports.rs
@@ -0,0 +1,9 @@
+// rustfmt-merge_imports: true
+// rustfmt-unnest_imports: true
+
+use a::b;
+use a::c;
+use a::d::e;
+use a::d::f;
+use a::d::{g, h::{i, j::k, l}};
+pub use a::d::m;
---
+++ tests/target/merge_and_unnest_imports.rs
@@ -0,0 +1,8 @@
+// rustfmt-merge_imports: true
+// rustfmt-unnest_imports: true
+
+use a::d::h::j::k;
+use a::d::h::{i, l};
+pub use a::d::m;
+use a::d::{e, f, g};
+use a::{b, c};
