6a32db163f4a7a58ec20fbf298788199ca2b0e6a
vallentin <mail@vallentin.dev>
2021-01-18 20:42:22 +0100

    Added 4654 test case

---
+++ tests/source/issue-4654.rs
@@ -0,0 +1,45 @@
+struct Foo {
+    bar: ()
+        // Comment
+}
+
+struct Bar {
+    baz: ()
+/*
+Comment
+*/
+}
+
+struct Baz(
+    ()
+        // Comment
+);
+
+fn main() {
+    let _ = Foo {
+        bar: ()
+                // Comment
+    };
+
+    let _ = Bar {
+        baz: ()
+    /*
+    Comment
+    */
+    };
+
+    let _ = Baz(
+        ()
+
+            // Comment
+    );
+
+    match a {
+0 => {}
+        // Foo
+    1 => {} // Bar
+        // Baz
+            2 => {}
+// Qux
+    }
+}
