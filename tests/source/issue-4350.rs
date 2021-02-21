12f01f18772ae148959b11f0e2e0c7f45150711b
WhizSid <whizsid@aol.com>
2021-02-22 03:23:16 +0530

    Tests #4350

---
+++ tests/source/issue-4350.rs
@@ -0,0 +1,11 @@
+macro_rules! mto_text_left {
+    ($buf:ident, $n:ident, $pos:ident, $state:ident) => {{
+        let cursor = loop {
+            state = match iter.next() {
+                    None      if $pos == DP::Start => break last_char_idx($buf),
+                            None /*some comment */    => break 0,
+            };
+        };
+        Ok(saturate_cursor($buf, cursor))
+    }};
+}
