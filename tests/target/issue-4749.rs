bb2e26269429ca984a182642de9b611f808eee2d
WhizSid <whizsid@aol.com>
2021-03-22 03:14:58 +0530

    Fix comment removing issue of unit types in struct fields

---
+++ tests/target/issue-4749.rs
@@ -0,0 +1,23 @@
+struct S {
+    x: (/* type */),
+}
+
+struct S2((/* type */));
+
+struct S3(
+    (
+        /* type
+        2 */
+    ),
+);
+
+struct S4(
+    (
+        // Line comment
+    ),
+);
+
+enum E {
+    Variant1 { x: (/* type */) },
+    Variant2((/* type */)),
+}
