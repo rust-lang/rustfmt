77193eb89f5cfafcf19a5730a95d8aea08dd1b64
Ashvin Arsakularatne <ashvin.arsakularatne@gmail.com>
2021-07-20 15:18:16 +0530

    test: add tests for match_arm_wrapping

---
+++ tests/target/configs/match_arm_wrapping/default.rs
@@ -0,0 +1,13 @@
+// rustfmt-match_arm_wrapping: Default
+// Wrap match-arms
+
+fn main() {
+    match lorem {
+        true => {
+            foooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo(x)
+        }
+        false => {
+            println!("{}", sit)
+        }
+    }
+}
