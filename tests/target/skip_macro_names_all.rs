c4f84a8ebfa9f940473587dd019767e5709418e3
Tom Milligan <tom@reinfer.io>
2022-06-18 13:19:05 +0100

    commit missed files

---
+++ tests/target/skip_macro_names_all.rs
@@ -0,0 +1,16 @@
+// rustfmt-skip_macro_names: ["*","items"]
+
+macro_rules! items {
+    ($($arg:item)*) => { $($arg)* };
+}
+
+// Should skip this invocation
+items!(
+        const _: u8 = 0;
+);
+
+// Should also skip this invocation, as the wildcard covers it
+use self::items as renamed_items;
+renamed_items!(
+        const _: u8 = 0;
+);
