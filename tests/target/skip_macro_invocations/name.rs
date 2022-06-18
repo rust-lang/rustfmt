4bca4208575863b94f139d130eb3398e2be3def2
Tom Milligan <tom@reinfer.io>
2022-06-18 15:51:36 +0100

    [review] add lots more tests

--- tests/target/skip_macro_invocations/name.rs
+++ tests/target/skip_macro_invocations/name.rs
@@ -1,16 +1,11 @@
 // rustfmt-skip_macro_invocations: ["items"]
 
-macro_rules! items {
-    ($($arg:item)*) => { $($arg)* };
-}
-
 // Should skip this invocation
 items!(
         const _: u8 = 0;
 );
 
 // Should not skip this invocation
-use self::items as renamed_items;
 renamed_items!(
     const _: u8 = 0;
 );
