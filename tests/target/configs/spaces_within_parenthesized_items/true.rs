dda41a6f5909024f46b96086fbbda674812aa1f1
Henrique Lopes <runry.melicias@gmail.com>
2022-07-09 21:54:45 +0100

    put space_within_parenthesized_items config tests on the correct directories

--- tests/target/configs/spaces_within_parenthesized_items/true.rs
+++ tests/target/configs/spaces_within_parenthesized_items/true.rs
@@ -2,13 +2,32 @@
 #[cfg( test )]
 fn foo1() {}
 
+fn foo1_a(/* comment */) {}
+
+fn foo1_b(/* comment */ /* comment */) {}
+
 fn foo2( arg1: i32 ) -> i32 {
+    0
+}
+
+fn foo2_a( /* comment */ arg1: i32 ) -> i32 {
+    0
+}
+
+fn foo2_b( arg1: i32 /*comment */ ) -> i32 {
     0
 }
 
 fn foo3<T, U>( arg1: T, arg2: U ) -> T {
     Dummy( 1 );
     let ( x, y ) = ( 1, 2 );
+    ( /* comment */ 1, 2 );
+    ( 1, /* comment */ 2 );
+    ( 1, 2 /* comment */ );
+    0
+}
+
+fn foo3_a<T, U>( arg1: T, /* comment */ arg2: U ) -> T {
     0
 }
 
@@ -18,10 +37,21 @@ fn foo4(
 ) {
 }
 
+fn foo4_a(
+    /* comment */
+    arggggggggggggggggggggggggggggggggggggggg1: i32,
+    arggggggggggggggggggggggggggggggggggggggg2: i32,
+) {
+}
+
 fn foo5() -> i32 {
     foo1();
+    foo1(/* comment */);
     foo2( 1 );
+    foo2( /* comment */ 1 );
+    foo2( 1 /* comment */ );
     foo3( 1, 2 );
+    foo3( 1, /* comment */ 2 );
     foo4(
         000000000000000000000001111111111,
         000000000000000000000002222222222,
