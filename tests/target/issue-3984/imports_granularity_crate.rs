69f2d1d3516bda86d29970a90f2cee639d78c6a6
Yacin Tmimi <yacintmimi@gmail.com>
2022-07-21 00:52:25 -0400

    add test for 3984
    This is fixed by backporting 3999

---
+++ tests/target/issue-3984/imports_granularity_crate.rs
@@ -0,0 +1,15 @@
+// rustfmt-imports_granularity: Crate
+
+// See https://github.com/rust-lang/rustfmt/issues/3984
+use a::{item /* comment */};
+use b::{
+    a,
+    // comment
+    item,
+};
+use c::item; /* comment */
+use d::item; // really long comment (with `use` exactly 100 characters) ____________________________
+
+use std::e::{/* it's a comment! */ bar /* and another */};
+use std::f::{/* it's a comment! */ bar};
+use std::g::{bar /* and another */};
