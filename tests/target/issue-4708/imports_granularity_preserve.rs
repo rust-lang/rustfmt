cd8dc7c519f01864f2b5553812e7848f26a88962
Yacin Tmimi <yacintmimi@gmail.com>
2022-07-21 00:53:06 -0400

    add test for 4708
    This is fixed by backporting 3999

---
+++ tests/target/issue-4708/imports_granularity_preserve.rs
@@ -0,0 +1,13 @@
+// rustfmt-imports_granularity: Preserve
+
+// See https://github.com/rust-lang/rustfmt/issues/4708
+pub use serenity::model::{
+    // channel::{Message, Reaction, ReactionType},
+    // gateway::Ready,
+    // guild::{Member, Role},
+    // id::{ChannelId, GuildId, MessageId, RoleId, UserId},
+    // permissions::Permissions,
+    prelude::*,
+    // user::User,
+};
+use {std::env::args /* this comment gets removed */};
