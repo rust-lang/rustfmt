90619b648413831b3e33947ca9792b966c2a953d
Martin Juarez <juarezmachadojorgemartin@gmail.com>
2022-07-20 21:55:44 -0400

    Improvements: Adding the test cases (source and target) for the issue 3071.

---
+++ tests/source/issue-3071.rs
@@ -0,0 +1,12 @@
+// indent_style=Visual
+
+struct A<T>
+    where T: Send
+{
+    x: u32,
+}
+
+impl<T> A<T> where T: Send
+{
+    fn foo() {}
+}
---
+++ tests/target/issue-3071.rs
@@ -0,0 +1,15 @@
+// indent_style=Visual
+
+struct A<T>
+where
+    T: Send,
+{
+    x: u32,
+}
+
+impl<T> A<T>
+where
+    T: Send,
+{
+    fn foo() {}
+}
