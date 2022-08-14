6106aefc3a37f291992658bb407413e28adc31b8
David Bar-On <david.cdb004@gmail.com>
2022-08-14 15:50:53 +0300

    Fix #4824 - multi comments cause internal error

--- src/missed_spans.rs
+++ src/missed_spans.rs
@@ -278,11 +278,10 @@ impl<'a> FmtVisitor<'a> {
                     let first_line = &subslice[..offset];
                     self.push_str(first_line);
                     self.push_str(&comment_indent.to_string_with_newline(self.config));
-
-                    let other_lines = &subslice[offset + 1..];
+                    let other_lines = &subslice[offset + 1..].trim_start();
                     let comment_str =
                         rewrite_comment(other_lines, false, comment_shape, self.config)
-                            .unwrap_or_else(|| String::from(other_lines));
+                            .unwrap_or_else(|| other_lines.to_string());
                     self.push_str(&comment_str);
                 }
             }
---
+++ tests/source/issue-4824/two.rs
@@ -0,0 +1,71 @@
+// rustfmt-version: Two
+
+//  Original issue test case
+
+#[cfg(windows)]
+fn main() {
+use common_util::get_version_for_rc;
+use std::env;
+use winres::{VersionInfo, WindowsResource};
+
+println!("cargo:rerun-if-changed=favicon.ico");
+println!("cargo:rerun-if-changed=Cargo.toml"); // rerun when version changed
+/*let profile = env::var("PROFILE").unwrap();
+if profile == "release"*/ {
+let mut res = WindowsResource::new();
+res.set_icon("favicon.ico");
+let version = get_version_for_rc!();
+res.set_version_info(VersionInfo::PRODUCTVERSION, version);
+res.set_version_info(VersionInfo::FILEVERSION, version);
+res.compile().unwrap();
+}
+}
+
+#[cfg(not(windows))]
+fn main() {}
+
+// Other test cases
+
+fn main() {
+let a = 1; // One line comment
+/* Block comment */
+true
+}
+
+fn main() {
+let a = 1; /* Block comment */
+// One line comment
+true
+}
+
+fn main() {
+let a = 1; // One line comment
+/* Block comment */
+}
+
+fn main() {
+let a = 1; /* Block comment */
+// One line comment
+}
+
+fn main() {
+// One line comment
+/* Block comment */
+true
+}
+
+fn main() {
+/* Block comment */
+// One line comment
+true
+}
+
+fn main() {
+// One line comment
+/* Block comment */
+}
+
+fn main() {
+/* Block comment */
+// One line comment
+}
