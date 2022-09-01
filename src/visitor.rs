d8793ae611e16abce30ed1ec62070ca742e9ec9a
Yacin Tmimi <yacintmimi@gmail.com>
2022-09-01 14:09:33 -0400

    Set `ctx.inside_macro` based on `FmtVisitor.parent_context`
    Fixes 5526
    
    When creating new contexts with `FmtVisitor::get_context` we set the
    value of `ctx.inside_macro` based on the `parent_context` if it exists.
    This helps us remember that we're inside of a macro when creating new
    `RewriteContext` from recursively created `FmtVisitors`.
    
    A similar issue to 5526 was reported in 3139, and remembering that we're
    inside of a macro in this way helps us go down a code path that resolved
    3139 and was implemented in PR 3142.

--- src/visitor.rs
+++ src/visitor.rs
@@ -1001,10 +1001,11 @@ impl<'b, 'a: 'b> FmtVisitor<'a> {
     }
 
     pub(crate) fn get_context(&self) -> RewriteContext<'_> {
+        let inside_macro = self.parent_context.map_or(false, |ctx| ctx.inside_macro());
         RewriteContext {
             parse_sess: self.parse_sess,
             config: self.config,
-            inside_macro: Rc::new(Cell::new(false)),
+            inside_macro: Rc::new(Cell::new(inside_macro)),
             use_block: Cell::new(false),
             is_if_else_block: Cell::new(false),
             force_one_line_chain: Cell::new(false),
