5d6399830fbecd28aa32c16886543e46f1ca32ee
Martin Juarez <juarezmachadojorgemartin@gmail.com>
2022-09-02 16:47:59 -0400

    Adding test cases for the issue.

---
+++ tests/source/issue-5525.rs
@@ -0,0 +1,7 @@
+pub struct SomeCallback(
+    pub extern "C" fn(
+        long_argument_name_to_avoid_wrap: u32,
+        second_long_argument_name: u32,
+        third_long_argument_name: u32,
+    ),
+);
