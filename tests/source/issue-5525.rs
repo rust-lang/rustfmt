d0cfb576e0784b3dd74ecb909f928ee5d56b0120
Martin Juarez <juarezmachadojorgemartin@gmail.com>
2022-09-21 07:53:42 -0400

    Removing the file test/source/issue-5525.rs, since it's not required for idempotence tests.

--- tests/source/issue-5525.rs
+++
@@ -1,9 +0,0 @@
-// rustfmt-version: Two
-
-pub struct SomeCallback(
-    pub extern "C" fn(
-        long_argument_name_to_avoid_wrap: u32,
-        second_long_argument_name: u32,
-        third_long_argument_name: u32,
-    ),
-);
