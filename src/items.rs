a86f4ccd82550aa7b000817e1f7b1038b24fbee3
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-10-03 02:07:44 +0900

    Improvements: Fix PreferSameLine brace_style with trailing_comma Vertical results.

--- src/items.rs
+++ src/items.rs
@@ -2751,6 +2751,7 @@ fn rewrite_where_clause_rfc_style(
     span_end: Option<BytePos>,
     span_end_before_where: BytePos,
     where_clause_option: WhereClauseOption,
+    brace_style: BraceStyle,
 ) -> Option<String> {
     let (where_keyword, allow_single_line) = rewrite_where_keyword(
         context,
@@ -2779,6 +2780,7 @@ fn rewrite_where_clause_rfc_style(
         span_end,
         where_clause_option,
         force_single_line,
+        brace_style,
     )?;
 
     // 6 = `where `
@@ -2850,6 +2852,7 @@ fn rewrite_bounds_on_where_clause(
     span_end: Option<BytePos>,
     where_clause_option: WhereClauseOption,
     force_single_line: bool,
+    brace_style: BraceStyle,
 ) -> Option<String> {
     let span_start = predicates[0].span().lo();
     // If we don't have the start of the next span, then use the end of the
@@ -2869,7 +2872,11 @@ fn rewrite_bounds_on_where_clause(
         span_end,
         false,
     );
-    let comma_tactic = if where_clause_option.suppress_comma || force_single_line {
+    let comma_tactic = if where_clause_option.suppress_comma
+        || force_single_line
+        || brace_style == BraceStyle::PreferSameLine
+            && context.config.trailing_comma() == SeparatorTactic::Vertical
+    {
         SeparatorTactic::Never
     } else {
         context.config.trailing_comma()
@@ -2916,6 +2923,7 @@ fn rewrite_where_clause(
             span_end,
             span_end_before_where,
             where_clause_option,
+            brace_style,
         );
     }
 
