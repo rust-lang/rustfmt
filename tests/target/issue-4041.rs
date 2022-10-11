6f9d8e3f9e40564107ec43c4a917d1df9e45212c
Thaqib Mohammed <thaqibdamani0@gmail.com>
2022-10-11 15:00:59 -0400

    add test

---
+++ tests/target/issue-4041.rs
@@ -0,0 +1,4 @@
+//! List:
+//! - Sub list:
+//!   + very long #1 blah blah blah blah blah blah blah blah blah blah blah blah
+//!   + very long #2 blah blah blah blah blah blah blah blah blah blah blah blah
