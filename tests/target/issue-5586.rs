77871a015111c89cbc4b36d7b194128e443e5ae8
amab8901 <amab8901@protonmail.com>
2022-11-29 06:26:17 +0100

    issue-5586: condensed the tests

---
+++ tests/target/issue-5586.rs
@@ -0,0 +1,120 @@
+
+fn main() {
+
+    // first sample
+    {{{{{
+        let push_ident = if let Some(&node_id) =
+            subgraph_nodes.get(pull_to_push_idx)
+        {
+            self.node_id_as_ident(node_id, false)
+        } else {
+            // Entire subgraph is pull (except for a single send/push handoff output).
+            assert_eq!(
+                1,
+                send_ports.len(),
+                "If entire subgraph is pull, should have only one handoff output."
+            );
+            send_ports[0].clone()
+        };
+    }}}}}
+
+    // second sample
+    {{{{{
+        let push_ident = if let Some(&node_id) =
+            subgraph_nodes.get(pull_to_push_idx)
+        {
+            self.node_id_as_ident(node_id, false)
+        } else {
+            // Entire subgraph is pull (except for a single send/push handoff output).
+            assert_eq!(
+                1,
+                send_ports.len(),
+                "If entire subgraph is pull, should have only one handoff output."
+            );
+            send_ports[0].clone()
+        };
+    }}}}}
+
+    // third sample
+    {{{{{
+        let push_ident = if let Some(&node_id) =
+            subgraph_nodes.get(pull_to_push_idx)
+        {
+            self.node_id_as_ident(node_id, false)
+        } else {
+            // Entire subgraph is pull (except for a single send/push handoff output).
+            assert_eq!(
+                1,
+                send_ports.len(),
+                "If entire subgraph is pull, should have only one handoff output."
+            );
+            send_ports[0].clone()
+        };
+    }}}}}
+
+    // fourth sample
+    {{{{{
+        let push_ident =
+            if let Some(&node_id) = subgraph_nodes.get(pull_to_push_idx) {
+                self.node_id_as_ident(node_id, false)
+            } else {
+                // Entire subgraph is pull (except for a single send/push handoff output).
+                assert_eq!(
+                    1,
+                    send_ports.len(),
+                    "If entire subgraph is pull, should have only one handoff output."
+                );
+                send_ports[0].clone()
+            };
+    }}}}}
+
+    // fifth sample
+    {{{{{
+        let push_ident =
+            if let Some(&node_id) = subgraph_nodes.get(pull_to_push_idx) {
+                self.node_id_as_ident(node_id, false)
+            } else {
+                // Entire subgraph is pull (except for a single send/push handoff output).
+                assert_eq!(
+                    1,
+                    send_ports.len(),
+                    "If entire subgraph is pull, should have only one handoff output."
+                );
+                send_ports[0].clone()
+            };
+    }}}}}
+
+    // sixth sample
+    {{{{{
+        let push_ident = if let Some(&node_id) =
+            subgraph_nodes.get(pull_to_push_idx)
+        {
+            self.node_id_as_ident(node_id, false)
+        } else {
+            // Entire subgraph is pull (except for a single send/push handoff output).
+            assert_eq!(
+                1,
+                send_ports.len(),
+                "If entire subgraph is pull, should have only one handoff output."
+            );
+            send_ports[0].clone()
+        };
+    }}}}}
+
+    // seventh sample
+    {{{{{
+        let push_ident = if let Some(&node_id) =
+            subgraph_nodes.get(pull_to_push_idx)
+        {
+            self.node_id_as_ident(node_id, false)
+        } else {
+            // Entire subgraph is pull (except for a single send/push handoff output).
+            assert_eq!(
+                1,
+                send_ports.len(),
+                "If entire subgraph is pull, should have only one handoff output."
+            );
+            send_ports[0].clone()
+        };
+    }}}}}
+}
