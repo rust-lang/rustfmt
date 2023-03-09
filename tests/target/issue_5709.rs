281802a52072de8e8b0371f5d31300fbbca08da8
Yacin Tmimi <yacintmimi@gmail.com>
2023-03-09 09:03:13 -0500

    Add `matches!` test case for issue 5709

---
+++ tests/target/issue_5709.rs
@@ -0,0 +1,10 @@
+// rustfmt-version: Two
+
+fn is_something(foo: Foo, bar: Bar) -> bool {
+    matches!(
+        (legacy_required_finality, signature_weight),
+        (LegacyRequiredFinality::Any, Insufficient | Weak | Strict)
+        | (LegacyRequiredFinality::Weak, Weak | Strict)
+        | (LegacyRequiredFinality::Strict, Strict)
+    )
+}
