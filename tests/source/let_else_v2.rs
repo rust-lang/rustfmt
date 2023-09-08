b49cd9266dee7ed03f9ac7c84559b9d474ef5d58
rhysd <lin90162@yahoo.co.jp>
2023-09-08 21:45:01 +0900

    Bring more let-else v2 test cases from v1

--- tests/source/let_else_v2.rs
+++ tests/source/let_else_v2.rs
@@ -3,14 +3,18 @@
 
 fn issue5901() {
     #[cfg(target_os = "linux")]
-    let Some(x) = foo else { return; };
+    let Some(x) = foo else { todo!() };
 
     #[cfg(target_os = "linux")]
     // Some comments between attributes and let-else statement
-    let Some(x) = foo else { return; };
+    let Some(x) = foo else { todo!() };
 
     #[cfg(target_os = "linux")]
     #[cfg(target_arch = "x86_64")]
+    let Some(x) = foo else { todo!() };
+
+    // The else block is multi-lined
+    #[cfg(target_os = "linux")]
     let Some(x) = foo else { return; };
 
     // The else block will be single-lined because attributes and comments before `let`
@@ -19,4 +23,34 @@ fn issue5901() {
     #[cfg(target_arch = "x86_64")]
     // Some comments between attributes and let-else statement
     let Some(x) = foo else { todo!() };
+
+    // Some more test cases for v2 formatting with attributes
+
+    #[cfg(target_os = "linux")]
+    #[cfg(target_arch = "x86_64")]
+    let Some(x) = opt
+    // pre else keyword line-comment
+    else { return; };
+
+    #[cfg(target_os = "linux")]
+    #[cfg(target_arch = "x86_64")]
+    let Some(x) = opt else
+    // post else keyword line-comment
+    { return; };
+
+    #[cfg(target_os = "linux")]
+    #[cfg(target_arch = "x86_64")]
+    let Foo {x: Bar(..), y: FooBar(..), z: Baz(..)} = opt else {
+        return;
+    };
+
+    #[cfg(target_os = "linux")]
+    #[cfg(target_arch = "x86_64")]
+    let Some(Ok((Message::ChangeColor(super::color::Color::Rgb(r, g, b)), Point { x, y, z }))) = opt else {
+        return;
+    };
+
+    #[cfg(target_os = "linux")]
+    #[cfg(target_arch = "x86_64")]
+    let Some(x) = very_very_very_very_very_very_very_very_very_very_very_very_long_expression_in_assign_rhs() else { return; };
 }
--- tests/target/let_else.rs
+++ tests/target/let_else.rs
@@ -257,21 +257,21 @@ fn issue5901() {
     #[cfg(target_os = "linux")]
     let Some(x) = foo
     else {
-        return;
+        todo!()
     };
 
     #[cfg(target_os = "linux")]
     // Some comments between attributes and let-else statement
     let Some(x) = foo
     else {
-        return;
+        todo!()
     };
 
     #[cfg(target_os = "linux")]
     #[cfg(target_arch = "x86_64")]
     let Some(x) = foo
     else {
-        return;
+        todo!()
     };
 
     // The else block will be multi-lined because attributes and comments before `let`
