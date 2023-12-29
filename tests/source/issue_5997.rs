7894f70d392c8b048adcc0e3c5fb2f0e5cc2eba3
Yacin Tmimi <yacintmimi@gmail.com>
2023-12-28 21:17:11 -0500

    Add test case for issue 5997

---
+++ tests/source/issue_5997.rs
@@ -0,0 +1,15 @@
+// rustfmt-version: Two
+
+// This formats with two spaces:
+pub struct Newtype(
+    /// Doc
+    #[doc()] //
+    pub  Vec<u8>,
+);
+
+// This formats with one:
+pub struct Newtype(
+    /// Doc
+    #[doc()]
+    pub Vec<u8>,
+);
