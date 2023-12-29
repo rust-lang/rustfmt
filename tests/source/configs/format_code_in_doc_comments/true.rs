d5732d8325cf244a18de3786fa4431bbb02d06d7
IVIURARY <haydn.murray1@yahoo.co.uk>
2023-12-29 13:18:24 +0000

    Add tests for reformatting doc comments

---
+++ tests/source/configs/format_code_in_doc_comments/true.rs
@@ -0,0 +1,14 @@
+// rustfmt-format_code_in_doc_comments: true
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let five = 5;
+///
+/// assert_eq!(6, add_one(5))
+/// ;
+/// ```
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
