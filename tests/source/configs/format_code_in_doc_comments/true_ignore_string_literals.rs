09302301a03bbcfce4aeb7378b8194cb1a6b0f51
IVIURARY <haydn.murray1@yahoo.co.uk>
2023-12-29 13:32:01 +0000

    test for string literals in doc comments

---
+++ tests/source/configs/format_code_in_doc_comments/true_ignore_string_literals.rs
@@ -0,0 +1,10 @@
+// rustfmt-format_code_in_doc_comments: true
+
+/// String literal test
+/// ```rust
+/// let expected = "\nfoo \n";
+/// assert_eq!(expected, r#"
+/// foo
+/// "#);
+/// ```
+pub fn main() {}
