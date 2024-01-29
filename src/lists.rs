1b464f9dd1648a3034bf838e61fa5f6df3353607
Yacin Tmimi <yacintmimi@gmail.com>
2024-01-28 23:24:55 -0500

    leverage `itemize_list` and `write_list` to recover trait bound comments
    Fixes 2055
    
    Now when using `version::Two`, rustfmt will permit comments within trait
    bounds and actually reformat them instead of refusing to rewrite the
    entire trait definition because there are comments in the bounds.

--- src/lists.rs
+++ src/lists.rs
@@ -206,6 +206,8 @@ impl ListItem {
 pub(crate) enum Separator {
     Comma,
     VerticalBar,
+    // Used to format trait bounds
+    Plus,
 }
 
 impl Separator {
@@ -215,6 +217,8 @@ impl Separator {
             Separator::Comma => 2,
             // 3 = ` | `
             Separator::VerticalBar => 3,
+            // 3 = ` + `
+            Separator::Plus => 3,
         }
     }
 }
