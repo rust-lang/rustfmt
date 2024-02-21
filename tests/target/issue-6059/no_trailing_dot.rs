e8ac06d4fa447e3dab8ba1cf56d4aa5951256534
MarcusGrass <marcus.grass@protonmail.com>
2024-02-21 20:55:18 +0100

    Add test case for floats without trailing dots

---
+++ tests/target/issue-6059/no_trailing_dot.rs
@@ -0,0 +1,3 @@
+fn float_range_no_trailing_dot_compacts() {
+    let _range = 3.0 / 2.0..4.0;
+}
--- tests/target/issue-6059/trailing_dot.rs
+++ tests/target/issue-6059/trailing_dot.rs
@@ -1,3 +1,3 @@
-fn float_range() {
+fn float_range_trailing_dot() {
     let _range = 3. / 2. ..4.;
 }
