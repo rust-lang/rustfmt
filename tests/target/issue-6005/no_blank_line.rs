93fe512e5307cf3e0951e0326195b25bdcd1932a
ding-young <lsyhime@snu.ac.kr>
2024-03-19 14:23:03 +0900

    Add multiple blank lines, no blank line test cases

---
+++ tests/target/issue-6005/no_blank_line.rs
@@ -0,0 +1,15 @@
+// rustfmt-version: Two
+pub fn main() {
+    match a {
+        #![deny(non_exhaustive_omitted_patterns)]
+        // test
+        Expr::Array(ExprArray { attrs, .. }) => None,
+        _ => None,
+    }
+
+    match b {
+        #![deny(non_exhaustive_omitted_patterns)]
+        Expr::Array(ExprArray { attrs, .. }) => None,
+        _ => None,
+    }
+}
