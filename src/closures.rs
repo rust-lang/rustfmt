b21c80cb2598bf477e61aba14085e4a7f14d53b9
Alexander Glusker <a.glusker@mgutm.ru>
2024-03-29 10:29:39 +0300

    Ignoring empty statements in closures. Resolve #6116

--- src/closures.rs
+++ src/closures.rs
@@ -108,13 +108,25 @@ fn get_inner_expr<'a>(
         if !needs_block(block, prefix, context) {
             // block.stmts.len() == 1 except with `|| {{}}`;
             // https://github.com/rust-lang/rustfmt/issues/3844
-            if let Some(expr) = block.stmts.first().and_then(stmt_expr) {
+            if let Some(expr) = iter_stmts_without_empty(&block.stmts)
+                .next()
+                .and_then(stmt_expr)
+            {
                 return get_inner_expr(expr, prefix, context);
             }
         }
     }
 
     expr
+}
+
+fn iter_stmts_without_empty(
+    stmts: &thin_vec::ThinVec<ast::Stmt>,
+) -> impl Iterator<Item = &ast::Stmt> {
+    stmts.iter().filter(|x| match x.kind {
+        crate::ast::StmtKind::Empty => false,
+        _ => true,
+    })
 }
 
 // Figure out if a block is necessary.
@@ -124,7 +136,7 @@ fn needs_block(block: &ast::Block, prefix: &str, context: &RewriteContext<'_>) -
     });
 
     is_unsafe_block(block)
-        || block.stmts.len() > 1
+        || iter_stmts_without_empty(&block.stmts).count() > 1
         || has_attributes
         || block_contains_comment(context, block)
         || prefix.contains('\n')
