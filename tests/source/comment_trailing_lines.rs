fd95d0f65692a0f6d3ae138f309e34969618095d
Ross Williams <ross@ross-williams.net>
2024-05-11 16:15:32 -0400

    Remove multiple trailing lines in comments
    Previously, rustfmt would remove one blank, trailing line in a comment
    every time it ran, so formatting was not idempotent if a comment had
    multiple trailing lines. Comments are only reformatted in this way if
    `comment::rewrite_comment_inner` is called, which is enabled by any of the
    config options `normalize_comments`, `wrap_comments`, or
    `format_code_in_doc_comments` (for doc comments only). Absent those
    config options, no trailing line reformatting occurs at all.
    
    In this commit, when the existing condition that detects a blank, trailing
    line is true, any preceding blank lines are removed from the reformatted
    comment. A source/target "system test" was added to prevent regression.
    
    Signed-off-by: Ross Williams <ross@ross-williams.net>

---
+++ tests/source/comment_trailing_lines.rs
@@ -0,0 +1,21 @@
+// rustfmt-wrap_comments: true
+
+//! Module comment with multiple trailing lines
+//!
+//!
+
+/// Doc comment with multiple trailing lines
+///
+///
+pub mod foo {}
+
+/// Doc comment with one trailing line
+///
+pub mod bar {}
+
+/*
+ * Block comment with multiple trailing lines
+ *
+ *
+ */
+pub mod block {}
