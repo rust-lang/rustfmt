595354690a7473cbad0c955b76753cc88d806c2f
benluiwj <wenjie.lui@gmail.com>
2024-06-06 20:42:13 +0800

    add repo cloning fn

--- check_diff/src/main.rs
+++ check_diff/src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use std::env;
+use std::process::Command;
+
 /// Inputs for the check_diff script
 #[derive(Parser)]
 struct CliInputs {
@@ -13,6 +16,17 @@ struct CliInputs {
     /// pass when running the feature branch
     #[arg(value_delimiter = ',', short, long, num_args = 1..)]
     rustfmt_config: Option<Vec<String>>,
+}
+
+fn clone_git_repo(url: String, dest: String) {
+    env::set_var("GIT_TERMINAL_PROMPT", "0");
+    let mut git_cmd = Command::new("git");
+    git_cmd.args(["clone", "--quiet", url, "--depth", "1", "dest"]);
+    git_cmd.output().expect("failed to clone repository");
+    let mut enter_dest_dir = Command::new("cd")
+        .arg(dest)
+        .output()
+        .expect("failed to enter directory.");
 }
 
 fn main() {
