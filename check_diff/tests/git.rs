d9d77fe41d659d7bae0e09214739b64f24459451
benluiwj <wenjie.lui@gmail.com>
2024-06-19 10:54:07 +0800

    update tests

--- check_diff/tests/git.rs
+++ check_diff/tests/git.rs
@@ -5,32 +5,39 @@ use std::fs;
 use std::path::Path;
 
 const TEMP_DIR_PATH: &str = "./tmp/";
+const HOME_PATH: &str = "./";
 
 // removes the previous tmp folder if it exists
-#[macro_export]
-macro_rules! cleanup {
-    () => {
-        if (Path::new(TEMP_DIR_PATH).exists()) {
-            let _ = fs::remove_dir_all(TEMP_DIR_PATH);
-        }
-    };
+fn cleanup() {
+    let _ = env::set_current_dir(HOME_PATH);
+    println!(
+        "Current directory: {}",
+        env::current_dir().unwrap().display()
+    );
+    let path = Path::new(TEMP_DIR_PATH);
+    println!("{}", path.exists());
+    if path.exists() {
+        let _ = fs::remove_dir_all(path);
+    }
 }
 
 #[test]
 fn clone_repo_test() {
+    cleanup();
     let sample_repo = "https://github.com/rust-lang/rustfmt.git";
     let dest_path = Path::new(TEMP_DIR_PATH);
     clone_git_repo(sample_repo, dest_path);
-    let directory = fs::read_dir(TEMP_DIR_PATH);
+    let directory = fs::read_dir(dest_path);
     // check whether we can read this directory
     assert_eq!(directory.is_err(), false);
     // check that the directory is non-empty
     assert_eq!(directory.iter().next().is_none(), false);
-    cleanup!();
+    cleanup();
 }
 
 #[test]
 fn cd_test() {
+    cleanup();
     let dest_path = Path::new(TEMP_DIR_PATH);
     let _ = fs::create_dir(dest_path);
     change_directory_to_path(dest_path);
@@ -39,5 +46,5 @@ fn cd_test() {
         env::current_dir().unwrap().file_name(),
         Some(OsStr::new("tmp"))
     );
-    cleanup!();
+    cleanup();
 }
