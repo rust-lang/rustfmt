90259f250c5edde9fb697bac9308a7a62df915c2
benluiwj <wenjie.lui@gmail.com>
2024-06-19 11:16:44 +0800

    cleanup common functions

--- check_diff/tests/common/mod.rs
+++ check_diff/tests/common/mod.rs
@@ -8,12 +8,7 @@ const HOME_PATH: &str = "./";
 // removes the previous tmp folder if it exists
 pub fn cleanup() {
     let _ = env::set_current_dir(HOME_PATH);
-    println!(
-        "Current directory: {}",
-        env::current_dir().unwrap().display()
-    );
     let path = Path::new(TEMP_DIR_PATH);
-    println!("{}", path.exists());
     if path.exists() {
         let _ = fs::remove_dir_all(path);
     }
