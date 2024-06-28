49b49da66025e67c3140035660ab747f250d8884
benluiwj <wenjie.lui@gmail.com>
2024-06-28 16:41:20 +0800

    add tempfile to tests

--- check_diff/tests/bash_commands.rs
+++ check_diff/tests/bash_commands.rs
@@ -1,23 +1,33 @@
 use check_diff::change_directory_to_path;
 use std::env;
 use std::ffi::OsStr;
-use std::fs;
-use std::path::Path;
-
-use common::{cleanup, TEMP_DIR_PATH};
-
-mod common;
+use tempfile::Builder;
 
 #[test]
 fn cd_test() {
-    cleanup();
-    let dest_path = Path::new(TEMP_DIR_PATH);
-    let _ = fs::create_dir(dest_path);
-    change_directory_to_path(dest_path);
-    assert_eq!(env::current_dir().is_ok(), true);
-    assert_eq!(
-        env::current_dir().unwrap().file_name(),
-        Some(OsStr::new("tmp"))
-    );
-    cleanup();
+    // create directory in current directory
+    let dir = Builder::new().tempdir_in("");
+
+    match dir {
+        Ok(d) => {
+            let dest_path = d.path();
+            let _ = change_directory_to_path(dest_path);
+            assert_eq!(env::current_dir().is_ok(), true);
+            assert_ne!(
+                env::current_dir().unwrap().file_name(),
+                Some(OsStr::new("check_diff"))
+            );
+            match d.close() {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("Error from closing: {}", e);
+                    assert_eq!(1, 2);
+                }
+            }
+        }
+        Err(e) => {
+            println!("Error from building: {}", e);
+            assert_eq!(1, 2);
+        }
+    };
 }
--- check_diff/tests/git.rs
+++ check_diff/tests/git.rs
@@ -1,23 +1,31 @@
 use check_diff::clone_git_repo;
 
 use std::fs;
-use std::path::Path;
-
-use common::{cleanup, TEMP_DIR_PATH};
-
-mod common;
+use tempfile::Builder;
 
 #[test]
 fn clone_repo_test() {
-    cleanup();
-    let sample_repo = "https://github.com/rust-lang/rustfmt.git";
-    let dest_path = Path::new(TEMP_DIR_PATH);
-    let result = clone_git_repo(sample_repo, dest_path);
-    assert_eq!(result.is_ok(), true);
-    let directory = fs::read_dir(dest_path);
-    // check whether we can read this directory
-    assert_eq!(directory.is_err(), false);
-    // check that the directory is non-empty
-    assert_eq!(directory.iter().next().is_none(), false);
-    cleanup();
+    let dir = Builder::new().tempdir_in("");
+    match dir {
+        Ok(d) => {
+            let sample_repo = "https://github.com/rust-lang/rustfmt.git";
+            let dest_path = d.path();
+            let result = clone_git_repo(sample_repo, dest_path);
+            assert_eq!(result.is_ok(), true);
+            let directory = fs::read_dir(dest_path);
+            // check whether we can read this directory
+            assert_eq!(directory.is_err(), false);
+            // check that the directory is non-empty
+            assert_eq!(directory.iter().next().is_none(), false);
+            match d.close() {
+                Ok(_) => {}
+                Err(_) => {
+                    assert_eq!(1, 2);
+                }
+            }
+        }
+        Err(_) => {
+            assert_eq!(1, 2);
+        }
+    }
 }
