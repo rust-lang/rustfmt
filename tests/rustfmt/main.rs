ab11149f3b3f8b2adc29881c987e43bbf3baea06
Gio Castro <giordan12@me.com>
2024-07-07 13:04:30 -0500

    Update test name to be more descriptive

--- tests/rustfmt/main.rs
+++ tests/rustfmt/main.rs
@@ -210,7 +210,7 @@ fn rustfmt_emits_error_when_control_brace_style_is_always_next_line() {
 
 #[nightly_only_test]
 #[test]
-fn rustfmt_generates_no_error() {
+fn rustfmt_generates_no_error_if_failed_format_code_in_doc_comments() {
     // See also https://github.com/rust-lang/rustfmt/issues/6109
 
     let file = "tests/target/issue-6109.rs";
