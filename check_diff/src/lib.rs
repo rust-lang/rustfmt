51a743e75982835055acc0f2c756f24f1164b621
benluiwj <wenjie.lui@gmail.com>
2024-08-13 15:16:41 +0800

    add ld lib path functionality

--- check_diff/src/lib.rs
+++ check_diff/src/lib.rs
@@ -1,10 +1,11 @@
 use git::clone_git_repo;
 use git::git_fetch;
 use git::git_remote_add;
-use git::GitError;
 use std::env;
 use std::io;
+use std::io::Error;
 use std::path::Path;
+use std::process::Command;
 use structs::CliInputs;
 use tracing::info;
 
@@ -27,25 +28,46 @@ pub fn change_directory_to_path(dest: &Path) -> io::Result<()> {
 // One for the current master, and another for the feature branch
 // Parameters:
 // dest: Directory where rustfmt will be cloned
-pub fn compile_rustfmt(dest: &Path, inputs: CliInputs) -> Result<(), GitError> {
-    let clone_repo_result = clone_git_repo(RUSTFMT_REPO, dest);
+pub fn compile_rustfmt(dest: &Path, inputs: CliInputs) -> io::Result<Command> {
+    let Ok(_) = clone_git_repo(RUSTFMT_REPO, dest) else {
+        return Err(Error::other("Error cloning repo while compiling rustfmt"));
+    };
 
-    if clone_repo_result.is_err() {
-        return clone_repo_result;
-    }
+    let Ok(_) = git_remote_add(inputs.remote_repo_url.as_str()) else {
+        return Err(Error::other(format!(
+            "Error adding remote from {} while compiling rustfmt",
+            inputs.remote_repo_url
+        )));
+    };
 
-    let remote_add_result = git_remote_add(inputs.remote_repo_url.as_str());
-    if remote_add_result.is_err() {
-        return remote_add_result;
-    }
-
-    let fetch_result = git_fetch(inputs.feature_branch.as_str());
-    if fetch_result.is_err() {
-        return fetch_result;
-    }
+    let Ok(_) = git_fetch(inputs.feature_branch.as_str()) else {
+        return Err(Error::other(format!(
+            "Error fetching from {} while compiling rustfmt",
+            inputs.feature_branch
+        )));
+    };
 
     let cargo_version = env!("CARGO_PKG_VERSION");
     info!("Compiling with {}", cargo_version);
 
-    return Ok(());
+    //Because we're building standalone binaries we need to set `LD_LIBRARY_PATH` so each
+    // binary can find it's runtime dependencies. See https://github.com/rust-lang/rustfmt/issues/5675
+    // This will prepend the `LD_LIBRARY_PATH` for the master rustfmt binary
+
+    let Ok(command) = std::process::Command::new("rustc")
+        .args(["--print", "sysroot"])
+        .output()
+    else {
+        return Err(Error::other("Error getting sysroot"));
+    };
+
+    let Ok(sysroot) = String::from_utf8(command.stdout) else {
+        return Err(Error::other("Error converting sysroot to string"));
+    };
+
+    let ld_lib_path = format!("{}/lib", sysroot.trim_end());
+    env::set_var("LD_LIBRARY_PATH", ld_lib_path);
+    let result = Command::new("ls");
+
+    return Ok(result);
 }
