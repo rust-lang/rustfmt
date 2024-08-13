e0253762e0b678a08cbbe5e84d54c553c889ae31
benluiwj <wenjie.lui@gmail.com>
2024-08-13 10:46:06 +0800

    improve git error handling

--- check_diff/src/git.rs
+++ check_diff/src/git.rs
@@ -4,6 +4,12 @@ use std::path::Path;
 use std::process::Command;
 use tracing::info;
 
+pub enum GitCommand {
+    Clone,
+    RemoteAdd,
+    Fetch,
+}
+
 pub enum GitError {
     FailedClone { stdout: Vec<u8>, stderr: Vec<u8> },
     FailedRemoteAdd { stdout: Vec<u8>, stderr: Vec<u8> },
@@ -21,17 +27,28 @@ impl From<io::Error> for GitError {
 fn git_runner(
     args: Vec<&str>,
     envs: HashMap<&str, &str>,
-    error: GitError,
+    command: GitCommand,
     log_message: &str,
 ) -> Result<(), GitError> {
     let git_cmd = Command::new("git").envs(&envs).args(args).output()?;
     // if the git command does not return successfully,
     //  any command on the repo will fail. So fail fast.
     if !git_cmd.status.success() {
-        let git_error = error {
-            stdout: git_cmd.stdout,
-            stderr: git_cmd.stderr,
+        let git_error = match command {
+            GitCommand::Clone => GitError::FailedClone {
+                stdout: git_cmd.stdout,
+                stderr: git_cmd.stderr,
+            },
+            GitCommand::Fetch => GitError::FailedFetch {
+                stdout: git_cmd.stdout,
+                stderr: git_cmd.stderr,
+            },
+            GitCommand::RemoteAdd => GitError::FailedRemoteAdd {
+                stdout: git_cmd.stdout,
+                stderr: git_cmd.stderr,
+            },
         };
+
         return Err(git_error);
     }
     info!("{}", log_message);
@@ -55,17 +72,17 @@ pub fn clone_git_repo(url: &str, dest: &Path) -> Result<(), GitError> {
     .to_vec();
     let env_vars = HashMap::from([("GIT_TERMINAL_PROMPT", "0")]);
     let log_message = "Successfully clone repository.";
-    git_runner(args, env_vars, GitError::FailedClone, log_message)
+    git_runner(args, env_vars, GitCommand::Clone, log_message)
 }
 
 pub fn git_remote_add(url: &str) -> Result<(), GitError> {
     let args = ["remote", "add", "feature", url].to_vec();
     let log_message = "Successfully added remote.";
-    git_runner(args, HashMap::new(), GitError::FailedRemoteAdd, log_message)
+    git_runner(args, HashMap::new(), GitCommand::RemoteAdd, log_message)
 }
 
 pub fn git_fetch(branch_name: &str) -> Result<(), GitError> {
     let args = ["fetch", "feature", branch_name].to_vec();
     let log_message = "Successfully fetched.";
-    git_runner(args, HashMap::new(), GitError::FailedFetch, log_message)
+    git_runner(args, HashMap::new(), GitCommand::Fetch, log_message)
 }
