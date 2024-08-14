c2a04475d620b08e3634a7e9c76200c771cf5055
benluiwj <wenjie.lui@gmail.com>
2024-08-14 18:34:14 +0800

    revert refactors

--- check_diff/src/lib.rs
+++ check_diff/src/lib.rs
@@ -1,18 +1,94 @@
-use git::clone_git_repo;
-use git::git_fetch;
-use git::git_remote_add;
 use std::env;
 use std::io;
 use std::io::Error;
 use std::path::Path;
 use std::process::Command;
-use structs::CliInputs;
 use tracing::info;
 
-pub mod git;
-pub mod structs;
+pub enum GitError {
+    FailedClone { stdout: Vec<u8>, stderr: Vec<u8> },
+    FailedRemoteAdd { stdout: Vec<u8>, stderr: Vec<u8> },
+    FailedFetch { stdout: Vec<u8>, stderr: Vec<u8> },
+    IO(std::io::Error),
+}
 
 const RUSTFMT_REPO: &str = "https://github.com/rust-lang/rustfmt.git";
+
+impl From<io::Error> for GitError {
+    fn from(error: io::Error) -> Self {
+        GitError::IO(error)
+    }
+}
+
+/// Clone a git repository
+///
+/// Parameters:
+/// url: git clone url
+/// dest: directory where the repo should be cloned
+pub fn clone_git_repo(url: &str, dest: &Path) -> Result<(), GitError> {
+    let git_cmd = Command::new("git")
+        .env("GIT_TERMINAL_PROMPT", "0")
+        .args([
+            "clone",
+            "--quiet",
+            url,
+            "--depth",
+            "1",
+            dest.to_str().unwrap(),
+        ])
+        .output()?;
+
+    // if the git command does not return successfully,
+    // any command on the repo will fail. So fail fast.
+    if !git_cmd.status.success() {
+        let error = GitError::FailedClone {
+            stdout: git_cmd.stdout,
+            stderr: git_cmd.stderr,
+        };
+        return Err(error);
+    }
+
+    info!("Successfully clone repository.");
+    return Ok(());
+}
+
+pub fn git_remote_add(url: &str) -> Result<(), GitError> {
+    let git_cmd = Command::new("git")
+        .args(["remote", "add", "feature", url])
+        .output()?;
+
+    // if the git command does not return successfully,
+    // any command on the repo will fail. So fail fast.
+    if !git_cmd.status.success() {
+        let error = GitError::FailedRemoteAdd {
+            stdout: git_cmd.stdout,
+            stderr: git_cmd.stderr,
+        };
+        return Err(error);
+    }
+
+    info!("Successfully added remote.");
+    return Ok(());
+}
+
+pub fn git_fetch(branch_name: &str) -> Result<(), GitError> {
+    let git_cmd = Command::new("git")
+        .args(["fetch", "feature", branch_name])
+        .output()?;
+
+    // if the git command does not return successfully,
+    // any command on the repo will fail. So fail fast.
+    if !git_cmd.status.success() {
+        let error = GitError::FailedFetch {
+            stdout: git_cmd.stdout,
+            stderr: git_cmd.stderr,
+        };
+        return Err(error);
+    }
+
+    info!("Successfully fetched.");
+    return Ok(());
+}
 
 pub fn change_directory_to_path(dest: &Path) -> io::Result<()> {
     let dest_path = Path::new(&dest);
@@ -28,22 +104,27 @@ pub fn change_directory_to_path(dest: &Path) -> io::Result<()> {
 // One for the current master, and another for the feature branch
 // Parameters:
 // dest: Directory where rustfmt will be cloned
-pub fn compile_rustfmt(dest: &Path, inputs: CliInputs) -> io::Result<Command> {
+pub fn compile_rustfmt(
+    dest: &Path,
+    remote_repo_url: String,
+    feature_branch: String,
+    rustfmt_config: Option<Vec<String>>,
+) -> io::Result<Command> {
     let Ok(_) = clone_git_repo(RUSTFMT_REPO, dest) else {
         return Err(Error::other("Error cloning repo while compiling rustfmt"));
     };
 
-    let Ok(_) = git_remote_add(inputs.remote_repo_url.as_str()) else {
+    let Ok(_) = git_remote_add(remote_repo_url.as_str()) else {
         return Err(Error::other(format!(
             "Error adding remote from {} while compiling rustfmt",
-            inputs.remote_repo_url
+            remote_repo_url
         )));
     };
 
-    let Ok(_) = git_fetch(inputs.feature_branch.as_str()) else {
+    let Ok(_) = git_fetch(feature_branch.as_str()) else {
         return Err(Error::other(format!(
             "Error fetching from {} while compiling rustfmt",
-            inputs.feature_branch
+            feature_branch
         )));
     };
 
