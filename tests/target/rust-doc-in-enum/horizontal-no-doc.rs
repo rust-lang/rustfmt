df5b6d9e0a8d13366c4681257df727fbcf9a70a9
Malik Olivier Boussejra <malik@boussejra.com>
2024-08-19 08:16:34 +0900

    test(rust-doc-in-enum): Add reproducing example for issue #5662
    [1] https://github.com/rust-lang/rustfmt/issues/5662

---
+++ tests/target/rust-doc-in-enum/horizontal-no-doc.rs
@@ -0,0 +1,6 @@
+enum MyType {
+    A { field1: bool, field2: bool },
+    B { field1: bool, field2: bool },
+    C { field1: bool, field2: bool },
+    D { field1: bool, field2: bool },
+}
---
+++ tests/target/rust-doc-in-enum/horizontal-with-doc.rs
@@ -0,0 +1,7 @@
+enum MyType {
+    A { field1: bool, field2: bool },
+    B { field1: bool, field2: bool },
+    /// OMG a comment
+    C { field1: bool, field2: bool },
+    D { field1: bool, field2: bool },
+}
