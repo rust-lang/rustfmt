7688b2006d50853556711b9ddd07cc01324b2c2b
Mattias Wallin <mattias.wallin@lidendata.com>
2024-09-01 10:57:52 +0200

    Fix `clippy::derivable_impls`

--- src/config/options.rs
+++ src/config/options.rs
@@ -155,8 +155,10 @@ pub enum ReportTactic {
 
 /// What Rustfmt should emit. Mostly corresponds to the `--emit` command line
 /// option.
+#[derive(Default)]
 #[config_type]
 pub enum EmitMode {
+    #[default]
     /// Emits to files.
     Files,
     /// Writes the output to stdout.
@@ -310,12 +312,6 @@ impl ::std::str::FromStr for WidthHeuristics {
     }
 }
 
-impl Default for EmitMode {
-    fn default() -> EmitMode {
-        EmitMode::Files
-    }
-}
-
 /// A set of directories, files and modules that rustfmt should ignore.
 #[derive(Default, Clone, Debug, PartialEq)]
 pub struct IgnoreList {
@@ -425,8 +421,10 @@ pub trait CliOptions {
 }
 
 /// The edition of the syntax and semantics of code (RFC 2052).
+#[derive(Default)]
 #[config_type]
 pub enum Edition {
+    #[default]
     #[value = "2015"]
     #[doc_hint = "2015"]
     /// Edition 2015.
@@ -443,12 +441,6 @@ pub enum Edition {
     #[doc_hint = "2024"]
     /// Edition 2024.
     Edition2024,
-}
-
-impl Default for Edition {
-    fn default() -> Edition {
-        Edition::Edition2015
-    }
 }
 
 impl From<Edition> for rustc_span::edition::Edition {
