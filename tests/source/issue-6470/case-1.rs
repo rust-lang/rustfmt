cb0c596a324a00c02360e8b293f46a1d990ffba8
Febriananda Wida Pramudita <febriwpramu@gmail.com>
2025-02-13 13:01:57 +0700

    add more testcase

--- tests/source/issue-6470/case-1.rs
+++ tests/source/issue-6470/case-1.rs
@@ -1,15 +1,44 @@
 // rustfmt-space_after_colon: false
 
 struct SomeStruct {
-    some_field: ::some_crate::Thing,
+    field1: ::some_crate::Thing,
+    field2 : ::some_crate::Thing,
+
+    field3:some_crate::Thing,
+    field4 :some_crate::Thing,
+    field5: some_crate::Thing,
+    field6 : some_crate::Thing,
+
+    field7:i32,
+    field8 :i32,
+    field9: i32,
+    field10 : i32,
 }
 
-const THING: ::some_crate::SomeType = ::some_crate::SomeType::default();
+const THING1: ::some_crate::SomeType = ::some_crate::SomeType::default();
+const THING2: ::some_crate::SomeType = ::some_crate::SomeType::default();
+
+const THING3: some_crate::SomeType = some_crate::SomeType::default();
+const THING4 :some_crate::SomeType = some_crate::SomeType::default();
+const THING5: some_crate::SomeType = some_crate::SomeType::default();
+const THING6 : some_crate::SomeType = some_crate::SomeType::default();
+
+const THING7: i32 = 0;
+const THING8 :i32 = 0;
+const THING9: i32 = 0;
+const THING10 : i32 = 0;
 
 fn main() {
-    let x: ::some_crate::SomeType = ::some_crate::SomeType::default();
-    let a1: int;
-    let a2:int;
-    let a3 :int;
-    let a4 : int;
+    let x1: ::some_crate::SomeType = ::some_crate::SomeType::default();
+    let x2: ::some_crate::SomeType = ::some_crate::SomeType::default();
+
+    let x3:some_crate::SomeType = ::some_crate::SomeType::default();
+    let x4 : some_crate::SomeType = ::some_crate::SomeType::default();
+    let x5: some_crate::SomeType = ::some_crate::SomeType::default();
+    let x6 : some_crate::SomeType = ::some_crate::SomeType::default();
+
+    let x7: i32 = 0;
+    let x8 :i32 = 0;
+    let x9: i32 = 0;
+    let x10 : i32 = 0;
 }
