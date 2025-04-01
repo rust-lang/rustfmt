7265718ead3263e3b4ca1fff47234d2be12d6ce4
Yacin Tmimi <yacintmimi@gmail.com>
2025-03-31 23:29:54 -0400

    Add `#![feature(ergonomic_clones)]` formatting
    Tracking issue r-l/rust#134797
    
    Fixes 6512

--- src/expr.rs
+++ src/expr.rs
@@ -161,10 +161,6 @@ pub(crate) fn format_expr(
         ast::ExprKind::Tup(ref items) => {
             rewrite_tuple(context, items.iter(), expr.span, shape, items.len() == 1)
         }
-        ast::ExprKind::Use(_, _) => {
-            // FIXME: properly implement this
-            Ok(context.snippet(expr.span()).to_owned())
-        }
         ast::ExprKind::Let(ref pat, ref expr, _span, _) => rewrite_let(context, shape, pat, expr),
         ast::ExprKind::If(..)
         | ast::ExprKind::ForLoop { .. }
@@ -276,6 +272,7 @@ pub(crate) fn format_expr(
         | ast::ExprKind::Field(..)
         | ast::ExprKind::MethodCall(..)
         | ast::ExprKind::Await(_, _)
+        | ast::ExprKind::Use(_, _)
         | ast::ExprKind::Yield(ast::YieldKind::Postfix(_)) => rewrite_chain(expr, context, shape),
         ast::ExprKind::MacCall(ref mac) => {
             rewrite_macro(mac, context, shape, MacroPosition::Expression).or_else(|_| {
