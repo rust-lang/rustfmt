a3fa7fb4b013553d0f7e30b965be9f0df8e6c60f
MarcusGrass <marcus.grass@protonmail.com>
2025-06-11 21:44:56 +0200

    fix: #6520, move comment above line if it would cross out the type in a function arg

---
+++ tests/target/issue_6520.rs
@@ -0,0 +1,24 @@
+pub fn foo(
+    // comment here
+    x: u32,
+) {
+}
+
+pub fn bar(
+    // comment here
+    x: u32,
+    y: i32,
+) {
+}
+
+pub fn baz(
+    my_arg1: i32,
+    my_arg2: u32,
+    my_arg3: bool,
+    // other comment here
+    my_arg4: f32,
+    my_arg5: String,
+    my_arg6: u32,
+    my_arg7: bool,
+) {
+}
