5c3b253a2871f94eba5d97b2d924d0bdf74a8926
Deadbeef <ent3rm4n@gmail.com>
2025-07-13 16:49:19 +0800

    parse `const trait Trait`

--- src/items.rs
+++ src/items.rs
@@ -1162,6 +1162,7 @@ pub(crate) fn format_trait(
     offset: Indent,
 ) -> RewriteResult {
     let ast::Trait {
+        constness,
         is_auto,
         safety,
         ident,
@@ -1172,7 +1173,8 @@ pub(crate) fn format_trait(
 
     let mut result = String::with_capacity(128);
     let header = format!(
-        "{}{}{}trait ",
+        "{}{}{}{}trait ",
+        format_constness(constness),
         format_visibility(context, &item.vis),
         format_safety(safety),
         format_auto(is_auto),
