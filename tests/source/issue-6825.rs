756e67c4e905186e9016753977acae8ca7d7695d
Azan Ali <azan.ali@arbisoft.com>
2026-03-24 12:02:03 +0500

    fixed formatting of commented single param in 2024

---
+++ tests/source/issue-6825.rs
@@ -0,0 +1,5 @@
+pub async fn foo(
+    // OriginalUri(original_uri): OriginalUri,
+) -> Option<Option<Option<Option<String>>>> {
+    None
+}
---
+++ tests/target/issue-6825.rs
@@ -0,0 +1,4 @@
+pub async fn foo(// OriginalUri(original_uri): OriginalUri,
+) -> Option<Option<Option<Option<String>>>> {
+    None
+}
