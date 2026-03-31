44ec2c49fe98ed8f1ff03e15b9267139a11738e2
Nicole LeGare <legaren@immunant.com>
2026-03-31 15:23:43 -0700

    Add more method chain test cases

--- tests/source/issue-4053.rs
+++ tests/source/issue-4053.rs
@@ -1,11 +1,26 @@
-// rustfmt-file_lines: [{"file":"tests/source/issue-4053.rs","range":[5,5]},{"file":"tests/source/issue-4053.rs","range":[13,13]},{"file":"tests/source/issue-4053.rs","range":[21,21]}]
+// rustfmt-file_lines: [{"file":"tests/source/issue-4053.rs","range":[8,8]},{"file":"tests/source/issue-4053.rs","range":[17,17]},{"file":"tests/source/issue-4053.rs","range":[23,23]},{"file":"tests/source/issue-4053.rs","range":[36,36]}]
 
 fn method_chain(val: Option<i32>) {
+    // Format one line of a short expression. If we were formatting the whole
+    // expression we would put it all on one line, so we need to handle it
+    // differently if we're formatting just one part of the expression.
     let _ = val
     .map(|val| val)
         .unwrap();
 
+    // Format one method call in a large method chain.
     let _ = val
+        .map(|val| val)
+    .map(|val| val).map(|val| val)
+        .map(|val| val)
+        .map(|val| val)
+    .map(|val| val)
+        .map(|val| val)
+            .map(|val| val)
+        .unwrap();
+
+    // Format the first part of a large method chain.
+    let _ = val.map(|val| val).map(|val| val)
         .map(|val| val)
     .map(|val| val).map(|val| val)
         .map(|val| val)
