eccd87b7fe5e376be661272a7cee279a66354f51
Nicole LeGare <legaren@immunant.com>
2026-03-31 15:04:10 -0700

    Flesh out method chain test

--- tests/source/issue-4053.rs
+++ tests/source/issue-4053.rs
@@ -1,8 +1,18 @@
-// rustfmt-file_lines: [{"file":"tests/source/issue-4053.rs","range":[5,5]},{"file":"tests/source/issue-4053.rs","range":[11,11]}]
+// rustfmt-file_lines: [{"file":"tests/source/issue-4053.rs","range":[5,5]},{"file":"tests/source/issue-4053.rs","range":[13,13]},{"file":"tests/source/issue-4053.rs","range":[21,21]}]
 
 fn method_chain(val: Option<i32>) {
     let _ = val
     .map(|val| val)
+        .unwrap();
+
+    let _ = val
+        .map(|val| val)
+    .map(|val| val).map(|val| val)
+        .map(|val| val)
+        .map(|val| val)
+    .map(|val| val)
+        .map(|val| val)
+            .map(|val| val)
         .unwrap();
 }
 
