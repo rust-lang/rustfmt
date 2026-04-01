2af341722940bc246381782e89167877743bb4d7
Nicole LeGare <legaren@immunant.com>
2026-04-01 14:47:30 -0700

    Add test for partially selecting a chain item

---
+++ tests/source/issue-4053-partial-item.rs
@@ -0,0 +1,19 @@
+// rustfmt-file_lines: [{"file":"tests/source/issue-4053-partial-item.rs","range":[7,7]},{"file":"tests/source/issue-4053-partial-item.rs","range":[12,13]}]
+
+fn method_chain(val: Option<i32>) {
+    let _ = val
+        .map(|val| val)
+    .map(|val| val).map(|val| val)
+            .map(|val| {
+                println!("...");
+                val
+            })
+        .map(|val| val)
+    .map(|val| {
+        println!("...");
+        val
+    })
+        .map(|val| val)
+            .map(|val| val)
+        .unwrap();
+}
