215efee9602e897c33352f1c73d7c547a30aeb12
Ly <echo.aimless@gmail.com>
2026-04-02 02:29:21 +0800

    test: target only test case for issue #6832

---
+++ tests/target/issue-6832.rs
@@ -0,0 +1,8 @@
+// rustfmt-format_code_in_doc_comments: true
+// rustfmt-fn_single_line: true
+// rustfmt-error_on_unformatted: true
+
+/// ```rust
+/// let font = String::from("hello");
+/// ```
+fn main() {}
