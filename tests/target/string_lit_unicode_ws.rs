27f418213c8469d26bf696233aaf35ebc364ed04
JojoFlex1 <whateveriffeel@gmail.com>
2026-04-11 07:37:00 +0300

    Revert regex change and update test with Unicode character

--- tests/target/string_lit_unicode_ws.rs
+++ tests/target/string_lit_unicode_ws.rs
@@ -1,6 +1,5 @@
-// Test that Unicode Pattern_White_Space characters are not stripped after line continuation
-
+// Test Unicode whitespace characters in string literal line continuation
 fn main() {
-    let str = "who is olaf\
-\u{00A0}This is a rust code example to show a bug in Unicode Pattern WhiteSpace";
+    let str = "hello \
+ world";
 }
