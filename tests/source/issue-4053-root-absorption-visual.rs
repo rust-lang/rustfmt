cbd206eb35bcdbaa6509837d367c4c96356de70c
Nicole LeGare <legaren@immunant.com>
2026-04-14 14:46:17 -0700

    Add same test but for visual formatting

---
+++ tests/source/issue-4053-root-absorption-visual.rs
@@ -0,0 +1,8 @@
+// rustfmt-indent_style: Visual
+// rustfmt-file_lines: [{"file":"tests/source/issue-4053-root-absorption-visual.rs","range":[5,5]}]
+
+fn method_chain(val: Option<i32>) {
+        val
+    .map(|val| val)
+        .unwrap();
+}
