48294f27ec200a6f6b5ab346fcaacf1b325e27fb
Taylor Ninesling <taylor.ninesling@gmail.com>
2026-07-27 20:43:54 -0400

    fix: prevent panic when whitespace separates `macro_rules` and `!`

--- src/macros.rs
+++ src/macros.rs
@@ -431,8 +431,18 @@ pub(crate) fn rewrite_macro_def(
     };
 
     let mut header = if def.macro_rules {
-        let pos = context.snippet_provider.span_after(span, "macro_rules!");
-        vec![HeaderPart::new("macro_rules!", span.with_hi(pos))]
+        let macro_rules_hi = context.snippet_provider.span_after(span, "macro_rules");
+        let bang_hi = context
+            .snippet_provider
+            .span_after(span.with_lo(macro_rules_hi), "!");
+        let header_span = span.with_hi(bang_hi);
+        let separator_span = mk_sp(macro_rules_hi, bang_hi - BytePos(1));
+        let header_snippet = if contains_comment(context.snippet(separator_span)) {
+            context.snippet(header_span)
+        } else {
+            "macro_rules!"
+        };
+        vec![HeaderPart::new(header_snippet, header_span)]
     } else {
         let macro_lo = context.snippet_provider.span_before(span, "macro");
         let macro_hi = macro_lo + BytePos("macro".len() as u32);
