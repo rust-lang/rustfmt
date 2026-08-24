7425b212ba33328fd334dcec7de4c9bdf36a6810
Jieyou Xu <jieyouxu@outlook.com>
2026-08-24 17:10:05 +0800

    [DiffCheck] Use `--locked` for cargo build

--- check_diff/src/lib.rs
+++ check_diff/src/lib.rs
@@ -580,7 +580,7 @@ pub fn build_rustfmt_from_src<T: AsRef<str>>(
     info!("Building rustfmt from source");
     let Ok(_) = Command::new("cargo")
         .current_dir(dir)
-        .args(["build", "-q", "--release", "--bin", "rustfmt"])
+        .args(["build", "--locked", "-q", "--release", "--bin", "rustfmt"])
         .output()
     else {
         return Err(CheckDiffError::FailedSourceBuild(
