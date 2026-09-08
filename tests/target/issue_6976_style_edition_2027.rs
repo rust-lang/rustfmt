271d4d8da1fe68764e9d9461cc9791f8ffceb0d9
Bryan Deng <bryan.deng002@gmail.com>
2026-09-08 16:53:37 -0400

    add test case with long type that doesn't fit on next line

--- tests/target/issue_6976_style_edition_2027.rs
+++ tests/target/issue_6976_style_edition_2027.rs
@@ -3,3 +3,8 @@
 pub(super) const ENCRYPTED_USERNAME_ENTROPY:
     [u8; usernames::constants::USERNAME_LINK_ENTROPY_SIZE] =
     const_str::hex!("4302c613c092a51c5394becffeb6f697300a605348e93f03c3db95e0b03d28f1");
+
+// type is really long that it doesn't fit on the next line
+pub(super) const ENCRYPTED_USERNAME_ENTROPY: [ThisTypeIsReallyLongAbcdefghikjlmnopqrstuvwxyz;
+    usernames::constants::USERNAME_LINK_ENTROPY_SIZE] =
+    const_str::hex!("4302c613c092a51c5394becffeb6f697300a605348e93f03c3db95e0b03d28f1");
