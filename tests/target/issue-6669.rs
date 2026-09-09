7fdb82b1e34cb016dc120e87875852ebbb23e23e
devangpratap <115096812+devangpratap@users.noreply.github.com>
2026-09-09 17:44:30 -0400

    Pair every block comment test case with a line comment one
    Address review: each `/* inline */` visibility case now has a `//`
    counterpart and vice versa, plus a comment that spans two lines in both
    forms.

--- tests/target/issue-6669.rs
+++ tests/target/issue-6669.rs
@@ -3,31 +3,66 @@
 
 pub(crate /* important comment */) fn a() {}
 
+pub(crate // important comment
+) fn a_line() {
+}
+
 pub(in /* which */ foo::bar) fn b() {}
+
+pub(in // which
+    foo::bar) fn b_line() {
+}
 
 pub(super /* up */) fn c() {}
 
+pub(super // up
+) fn c_line() {
+}
+
 pub(self /* here */) fn d() {}
 
+pub(self // here
+) fn d_line() {
+}
+
 pub(/* leading */ crate) fn e() {}
+
+pub(// leading
+    crate) fn e_line() {
+}
 
 pub(crate) fn no_comment() {}
 
 pub(crate /* mod */) mod m {}
 
+pub(crate // mod
+) mod m_line {}
+
 pub(crate /* use */) use std::io;
+
+pub(crate // use
+) use std::fmt;
 
 struct S {
     pub(crate /* field */) field: u32,
+
+    pub(crate // field
+    ) field_line: u32,
 }
 
-// Multi-line comments are kept as-is too.
+// Comments that span more than one line are kept as-is too.
 pub(crate /* multi
    line */) fn multi_line_block() {
 }
 
-pub(crate //  line
-) fn line_comment() {
+pub(crate // multi
+    // line
+) fn multi_line_single() {
 }
 
 pub(in /* one */ foo::bar /* two */) fn two_comments() {}
+
+pub(in foo::bar // one
+    // two
+) fn two_comments_line() {
+}
