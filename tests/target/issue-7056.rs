0e3523a754567bd52a1df0654d3efc5c594211fd
devangpratap <115096812+devangpratap@users.noreply.github.com>
2026-09-08 22:41:56 -0400

    test: use the full issue-7056 snippet, with an unformatted source file

--- tests/target/issue-7056.rs
+++ tests/target/issue-7056.rs
@@ -4,12 +4,8 @@
 
 #![feature(decl_macro)]
 
-macro_rules! r#struct {
+macro_rules!  r#struct  {
         ($r#struct:expr)  =>  {  $r#struct  }
-}
-
-macro r#decl_macro($r#fn:expr) {
-        $r#fn
 }
 
 macro_rules! old_macro {
@@ -18,8 +14,34 @@ macro_rules! old_macro {
     };
 }
 
-fn raw_ident_next_to_a_raw_string() {
-    let r#match = r#"still a raw string"#;
-    let r#fn = r##"hashes too"##;
-    let _ = (r#match, r#fn);
+macro  r#decl_macro($r#fn:expr)  {
+        $r#fn
+}
+
+macro passthrough($id:ident) {
+    $id
+}
+
+macro_rules! test_pat_match {
+    (a) => {
+        6
+    };
+    (r#a) => {
+        7
+    };
+}
+
+pub fn main() {
+    r#println!("{struct}", r#struct = 1);
+    assert_eq!(2, r#struct!(2));
+    assert_eq!(3, r#old_macro!(3));
+    assert_eq!(4, decl_macro!(4));
+
+    let r#match = 5;
+    assert_eq!(5, passthrough!(r#match));
+
+    assert_eq!("r#struct", stringify!(r#struct));
+
+    assert_eq!(6, test_pat_match!(a));
+    assert_eq!(7, test_pat_match!(r#a));
 }
